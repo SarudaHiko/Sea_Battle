61252f20d8eec5bb5198a88ae90d003ef0de2f9b
Saruda <98002516+SarudaHiko@users.noreply.github.com>
2023-02-16 03:10:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ class Player:
 # Включение ИИ в игру. 
 class AI(Player):
     def ask(self):
-        d = Dot(randint(0, 5), randint(0, 5))
+        d = Dot(randint(0, 9), randint(0, 9))
         print(f"Ход компьютера: {d.x + 1} {d.y + 1}")
         return d
 
